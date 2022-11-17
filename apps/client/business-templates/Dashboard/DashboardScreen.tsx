49226bd802f2c2a0407822abe1b8fca9d8ddf812
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2022-11-17 11:03:14 -0500

    chore: wip

--- apps/client/business-templates/Dashboard/DashboardScreen.tsx
+++ apps/client/business-templates/Dashboard/DashboardScreen.tsx
@@ -1,8 +1,14 @@
 import { Box, HStack, VStack } from "native-base"
 import React from "react"
+import { useIsSsr } from "../../hooks"
+import { AreaChart } from "./Ghaphs/AreaChart"
+import { PieChart } from "./Ghaphs/PieChart"
+import { VerticalBar } from "./Ghaphs/VerticalBar"
 
 export const DashboardScreen = () => {
-  return (
+
+
+  return useIsSsr() ? null : (
     <Box flex={1}>
 
       <Box
@@ -18,15 +24,33 @@ export const DashboardScreen = () => {
           borderWidth={0.5}
           borderColor={"gray.50"}
           shadow={"2"}
-          h={"300px"}
           w={"100%"}
           borderRadius={"md"}
-          bgColor={"white"}>
+          bgColor={"white"}
+          justifyContent={"center"}
+        >
+          <AreaChart />
         </Box>
 
-        <HStack space={3} h={"100%"} w={"100%"} flex={1} flexDirection={"row"}>
-          <Box borderWidth={0.5} borderColor={"gray.50"} shadow={"2"} flex={1} borderRadius={"md"} bgColor={"white"}></Box>
-          <Box borderWidth={0.5} borderColor={"gray.50"} shadow={"2"} flex={1} borderRadius={"md"} bgColor={"white"}></Box>
+        <HStack space={3} w={"100%"} flexDirection={"row"} flex={1}>
+          <Box borderWidth={0.5}
+            borderColor={"gray.50"}
+            shadow={"2"}
+            borderRadius={"md"}
+            bgColor={"white"}
+            p={2}
+            flex={1}
+          >
+            <VerticalBar />
+          </Box>
+          <Box borderWidth={0.5}
+            borderColor={"gray.50"}
+            shadow={"2"}
+            borderRadius={"md"}
+            bgColor={"white"}
+          >
+            <PieChart />
+          </Box>
         </HStack>
 
       </VStack>
