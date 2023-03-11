84fb0b75a2b3bca3e654a847884fbb4c9c2d1573
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2023-03-11 18:41:07 -0300

    chore: move QR code reader to a separate file

--- apps/client/client-templates/HomeScreen/HomeScreen.tsx
+++ apps/client/client-templates/HomeScreen/HomeScreen.tsx
@@ -1,64 +1,15 @@
-import { Image, Box, Button, Center, Heading, VStack, Text } from 'native-base'
-import NextImage from 'next/image'
-import React from 'react'
+import { Image, Box, Button, Center, Heading, VStack } from 'native-base'
+import React, { useState } from 'react'
 import { NavigationButton } from '../../components/atoms/NavigationButton'
-import { CustomModal } from '../../components/CustomModal/CustomModal'
-import { useHtml5QrCodeScanner } from 'react-html5-qrcode-reader';
-import { QrReader } from 'react-qr-reader';
 import { useRouter } from 'next/router'
-
-
-const texts = {
-  openNewTab: 'Open a New Tab',
-  joinTab: 'Join a Tab',
-  viewMenu: 'View Menu',
-  cancel: 'Cancel',
-}
-
-const handleOpenCamera = () => {
-  console.log("handle open camera");
-  if (
-    "mediaDevices" in navigator &&
-    "getUserMedia" in navigator.mediaDevices
-  ) {
-    const video = document.createElement("video");
-    const canvas = document.getElementById("camera");
-
-    // if canvas already have children, return
-    if (canvas?.children.length) return;
-
-    console.log({ canvas, video });
-    canvas?.appendChild(video);
-    video.setAttribute("playsinline", "");
-    video.setAttribute("autoplay", "");
-    video.setAttribute("muted", "");
-    video.style.width = "100%";
-    video.style.height = "100%";
-    video.style.borderRadius = "5px";
-    video.style.top = "0";
-    video.style.zIndex = "99999999999999";
-
-    console.log("Let's get this party started");
-    navigator.mediaDevices
-      .getUserMedia({
-        video: {
-          facingMode: "environment",
-        },
-      })
-      .then(function success(stream) {
-        console.log({ stream });
-        video.srcObject = stream;
-      });
-  }
-};
+import { texts } from './texts'
+import { QRCodeReader } from './QRCodeReader'
 
 export const HomeScreen = () => {
-  const [isJoinTabModalOpen, setIsJoinTabModalOpen] = React.useState(false)
-  const [qrCode, setQrCode] = React.useState<string | null>(null)
+
+  const [isJoinTabModalOpen, setIsJoinTabModalOpen] = useState(false)
   const route = useRouter()
 
-
-  // join a tab will open the camera and scan the QR code
   const joinTab = () => {
     console.log("Pressed")
     setIsJoinTabModalOpen(true)
@@ -106,38 +57,9 @@ export const HomeScreen = () => {
           onPress={() => console.log("Home")}
         />
       </Box>
-      {/* Add modal here */}
-      <CustomModal
+      <QRCodeReader
         isOpen={isJoinTabModalOpen}
-        HeaderComponent={"Scan the QR code to join a tab"}
-        ModalBody={
-          <Box width={"100%"} >
-            <QrReader
-              videoContainerStyle={{ borderRadius: 10, borderWidth: 1, borderColor: "black" }}
-              videoStyle={{ borderRadius: 10, borderWidth: 1, borderColor: "black" }}
-              containerStyle={{ borderRadius: 10, borderWidth: 1, borderColor: "black" }}
-              constraints={{ facingMode: "environment" }}
-              onResult={(result, error) => {
-                if (!!result) {
-                  setQrCode(result.getText());
-                  console.info(result)
-                }
-              }}
-            />
-            <Text>
-              {qrCode}
-            </Text>
-          </Box>
-        }
-        ModalFooter={
-          <Button
-            onPress={() => setIsJoinTabModalOpen(false)}
-            _text={{ bold: true }}
-            colorScheme={"trueGray"}
-          >
-            {texts.cancel}
-          </Button>
-        }
+        setModalVisibility={setIsJoinTabModalOpen}
       />
     </Center>
   )
