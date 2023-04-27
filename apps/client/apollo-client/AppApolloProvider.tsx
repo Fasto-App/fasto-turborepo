ef9554430500ecd5209be9c3c5e1d3a63fefe71c
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2023-04-27 00:27:58 -0300

    chore: revert change

--- apps/client/apollo-client/AppApolloProvider.tsx
+++ apps/client/apollo-client/AppApolloProvider.tsx
@@ -11,8 +11,7 @@ import { errorLink } from './ErrorLink';
 import { useRouter } from 'next/router';
 
 const httpLink = createUploadLink({
-  // uri: process.env.BACKEND_URL,
-  uri: "http://192.168.15.76:4000/graphql",
+  uri: process.env.BACKEND_URL,
 })
 
 export const AppApolloProvider: React.FC = ({ children }) => {
