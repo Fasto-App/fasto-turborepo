3f4d2312fe59ad75f94c42e6a705d4e904ba507d
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2023-11-29 21:02:05 -0500

    chore: stop from sending development errors

--- apps/server/src/bugsnag/bugsnag.ts
+++ apps/server/src/bugsnag/bugsnag.ts
@@ -5,6 +5,7 @@ Bugsnag.start({
   apiKey: process.env.BUGSNAG_KEY || '1a33a03202febef0248d4e0c63c0b9f4',
   plugins: [BugsnagPluginExpress],
   releaseStage: process.env.ENVIRONMENT,
+  enabledReleaseStages: ['production', 'staging'],
   appVersion: '0.4.0'
 })
 
