62470e248c81d4d1653c9bf19193f35b067aa1b4
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2023-11-30 14:59:51 -0500

    chore: Chart.register stays out of the component

--- apps/client/business-templates/Dashboard/Graphs/VerticalBar.tsx
+++ apps/client/business-templates/Dashboard/Graphs/VerticalBar.tsx
@@ -12,17 +12,18 @@ import { Bar } from 'react-chartjs-2';
 import { Box } from 'native-base';
 import { useTranslation } from "next-i18next";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 export function VerticalBar() {
   const { t } = useTranslation("businessDashboard");
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-  );
-  
+
   const options = {
     responsive: true,
     plugins: {
@@ -35,9 +36,9 @@ export function VerticalBar() {
       },
     },
   };
-  
+
   const labels = [t('january'), t('february'), t('march'), t('april'), t('may'), t('june'), t('july'), t('august'), t('september'), t('october'), t('november'), t('december')];
-  
+
   const data = {
     labels,
     datasets: [
