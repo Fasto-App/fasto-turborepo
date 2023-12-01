57a6815d77f4dc81c378a9ef28ad79d0f7228dbd
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2023-11-30 23:35:48 -0500

    chore: fix translation, remove unused code, change template PR

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -1,20 +1,22 @@
+# Name the Task
+
 ## Link To The Notion Ticket
-<link.com/> 
+
+[Notion Link](https://link-url-here.org)
 
 ## Description
 
 Please provide a brief overview of the changes that have been made and the purpose of the merge request.
+E.g: Update the layout for the top container on Orders Screen
 
 ## Changes Made
 
-- List the specific changes that have been made in bullet point format
-- Provide any relevant details or context for each change
+-   List the specific changes that have been made in bullet point format
+-   Provide any relevant details or context for each change
 
-## Checklist
+    E.g: - Added a dropdown to filter the data by type
 
-- [ ] Code has been reviewed and approved by at least one other member of the team
-- [ ] Any necessary documentation has been updated
+## Checklist for REVIEWERS
 
-## Additional Information
-
-- Provide any additional information or context that may be relevant for this merge request
+-   [ ] Code has been reviewed and approved by at least one other member of the team
+-   [ ] Any necessary documentation has been updated
--- apps/client/business-templates/Dashboard/Graphs/PieChart.tsx
+++ apps/client/business-templates/Dashboard/Graphs/PieChart.tsx
@@ -1,40 +1,11 @@
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
-import { Box, Button, Center, HStack, VStack, Text, Image, Heading, ScrollView } from 'native-base';
+import { Box, HStack, VStack, Text, Image, Heading, ScrollView } from 'native-base';
 import { PRODUCT_PLACEHOLDER_IMAGE, parseToCurrency } from 'app-helpers';
-import { PriceTag } from '../../../components/molecules/PriceTag';
 import { useTranslation } from "next-i18next";
-import { GetMostSellingProductsQuery, useGetMostSellingProductsQuery } from '../../../gen/generated';
+import { GetMostSellingProductsQuery } from '../../../gen/generated';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-
-export const data = {
-  labels: ['Pastel', 'Nata', 'Chicken', 'Peas', 'Coke'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
 
 const OrdersRow = ({
   orderType, number
