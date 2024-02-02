5535f0bc3592c18b2bf6a2a2b194ac15371292ae
Silas Roberto Mariosa de Vasconcelos <113197484+silas-mariosa@users.noreply.github.com>
2024-02-02 19:28:59 -0300

    chore: added lib react-image-file-resize in Paste Hooks

--- apps/client/hooks/index.ts
+++ apps/client/hooks/index.ts
@@ -2,18 +2,25 @@ import { useBreakpointValue } from "native-base";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { clearClientCookies, getClientCookies } from "../cookies";
-import { RequestStatus, TabStatus, useGetBusinessByIdQuery, useGetClientSessionQuery } from "../gen/generated";
+import {
+  RequestStatus,
+  TabStatus,
+  useGetBusinessByIdQuery,
+  useGetClientSessionQuery,
+} from "../gen/generated";
 import { customerRoute } from "fasto-route";
 import { showToast } from "../components/showToast";
 import { texts } from "./texts";
 import { getCause } from "../apollo-client/ErrorLink";
 import { useTranslation } from "next-i18next";
+import { resizeFile } from "./helper";
+
 
 export const useIsSsr = () => {
   const [isSsr, setIsSsr] = useState(true);
   useEffect(() => setIsSsr(false), []);
   return isSsr;
-}
+};
 
 export const useNumOfColumns = (showTilesList: boolean) => {
   const numColumns: number = useBreakpointValue({
@@ -22,53 +29,49 @@ export const useNumOfColumns = (showTilesList: boolean) => {
     md: showTilesList ? 1 : 2,
     lg: showTilesList ? 2 : 3,
     xl: showTilesList ? 2 : 3,
-    "2xl": showTilesList ? 3 : 4
+    "2xl": showTilesList ? 3 : 4,
   });
 
   return numColumns;
-}
-
+};
 
 export const useUploadFileHook = () => {
-  const [imageSrc, setImageUrl] = useState("")
-  const [imageFile, setImageFile] = useState<File>()
+  const [imageSrc, setImageUrl] = useState('');
+  const [imageFile, setImageFile] = useState<File | undefined>();
 
-
-  const handleFileOnChange = (evt: any) => {
-    const file = evt?.target?.files?.[0]
-
-    console.log("handleFileOnChange")
-    console.log("file", file)
+  const handleFileOnChange = async (evt: any) => {
+    const file = evt?.target?.files?.[0];    
 
     if (!file) {
-      setImageUrl("")
-      setImageFile(undefined)
-      return
+      setImageUrl('');
+      setImageFile(undefined);
+      return;
     }
 
-    const reader = new FileReader()
-
-    reader.readAsDataURL(file)
-    reader.onload = (e) => {
-      const url = e.target?.result
-      setImageUrl(url as string)
-      setImageFile(file)
+    try {
+      const { base64String, resizedFile } = await resizeFile(file);
+      console.log('resized image:', base64String);
+      setImageUrl(base64String);
+      setImageFile(new File([resizedFile], 'resized.webp', { type: 'image/webp' }));
+    } catch (err) {
+      console.log('Error resizing image:', err);
     }
-  }
+  };
 
   return {
     imageSrc,
     imageFile,
-    handleFileOnChange
-  }
-}
+    handleFileOnChange,
+  };
+};
+
+
 
 export const useGetClientSession = () => {
-  const route = useRouter()
-  const { businessId, checkoutId } = route.query
+  const route = useRouter();
+  const { businessId, checkoutId } = route.query;
 
-  const token = getClientCookies(businessId as string)
-
+  const token = getClientCookies(businessId as string);
 
   return useGetClientSessionQuery({
     skip: !token,
@@ -78,61 +81,64 @@ export const useGetClientSession = () => {
       // then we need to get a new token
       if (data.getClientSession.request.status === RequestStatus.Rejected) {
         if (businessId) {
-          const business = typeof businessId === "string" ? businessId : businessId[0]
+          const business =
+            typeof businessId === "string" ? businessId : businessId[0];
 
-          clearClientCookies(business)
+          clearClientCookies(business);
           return route.push({
             pathname: customerRoute["/customer/[businessId]"],
             query: {
-              businessId: business
-            }
-          })
+              businessId: business,
+            },
+          });
         }
       }
 
-      if (!checkoutId && data.getClientSession.tab?.status === TabStatus.Pendent
-        && data.getClientSession.tab.checkout
-        && route.pathname !== customerRoute["/customer/[businessId]/payment"]
+      if (
+        !checkoutId &&
+        data.getClientSession.tab?.status === TabStatus.Pendent &&
+        data.getClientSession.tab.checkout &&
+        route.pathname !== customerRoute["/customer/[businessId]/payment"] &&
         // todo: should Trust the user that navigate to this page?
-        && route.pathname !== customerRoute["/customer/[businessId]/success"]
+        route.pathname !== customerRoute["/customer/[businessId]/success"]
       ) {
         return route.push({
-          pathname: customerRoute["/customer/[businessId]/checkout/[checkoutId]"],
+          pathname:
+            customerRoute["/customer/[businessId]/checkout/[checkoutId]"],
           query: {
             businessId: businessId,
-            checkoutId: data.getClientSession.tab.checkout
-          }
-        })
-
+            checkoutId: data.getClientSession.tab.checkout,
+          },
+        });
       }
     },
-  })
-}
+  });
+};
 
 export const useGetBusinessInformation = () => {
-  const route = useRouter()
-  const { businessId } = route.query
+  const route = useRouter();
+  const { businessId } = route.query;
 
-  const { t } = useTranslation('common')
+  const { t } = useTranslation("common");
 
   const data = useGetBusinessByIdQuery({
     skip: !businessId,
     variables: {
       input: {
-        _id: businessId as string
-      }
+        _id: businessId as string,
+      },
     },
     onCompleted: (data) => {
-      console.log("data", data)
+      console.log("data", data);
     },
     onError: (error) => {
       showToast({
         message: t("thereWasAnError"),
         subMessage: getCause(error),
-        status: "error"
-      })
-    }
-  })
+        status: "error",
+      });
+    },
+  });
 
-  return data
-}
+  return data;
+};
