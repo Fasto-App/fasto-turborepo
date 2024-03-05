6db35058428fdb928451a02d122bd6ca70a1fb02
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-03-05 20:14:34 -0300

    feat: Notification schema

---
+++ apps/server/src/models/notification.ts
@@ -0,0 +1,35 @@
+import { Connection } from 'mongoose'
+import { prop, getModelForClass } from '@typegoose/typegoose';
+import type { AppNavigation } from 'fasto-route';
+import { Business } from './business';
+import type { Ref } from '@typegoose/typegoose';
+import { User } from './user';
+
+export class Notification {
+  @prop({ required: true })
+  public message!: string;
+
+  @prop()
+  public path?: AppNavigation;
+
+  @prop({ required: true, default: false })
+  public isRead!: boolean;
+
+  @prop({ ref: () => Business })
+  public business_receiver_id?: Ref<Business>;
+
+  @prop({ ref: () => User })
+  public customer_receiver_id!: Ref<User>;
+
+  @prop({ ref: () => User, required: true })
+  public sender_id!: Ref<User>;
+
+  @prop({ default: Date.now() })
+  public created_date!: Date;
+}
+
+export const NotificationModel = (conn: Connection) =>
+  getModelForClass(Notification, {
+    existingConnection: conn,
+    schemaOptions: { collection: 'Notification' }
+  });
