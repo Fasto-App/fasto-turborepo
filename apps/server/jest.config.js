8f4ec44798f5d568aa572f15a4ec5417d1d8e592
Aaron Smith <arnhsmith@gmail.com>
2024-04-01 19:58:56 -0400

    added coverage

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -3,5 +3,9 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
 preset: '@shelf/jest-mongodb',
 transform: tsjPreset.transform,
-
+coverageDirectory: './coverage',
+collectCoverageFrom: [
+    './src/**/*.ts',
+],
+collectCoverage: true,
 }
