478e3266f0393072684bee76d6b6ac8d28fe0708
Aaron Smith <arnhsmith@gmail.com>
2024-04-01 19:42:21 -0400

    added working test for notifications

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -1,24 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+preset: '@shelf/jest-mongodb',
+transform: tsjPreset.transform,
 
-// use the transform format below to get access mongodb and typescript
-
-
-// const { defaults: tsjPreset } = require('ts-jest/presets');
-// module.exports = {
-// preset: '@shelf/jest-mongodb',
-// transform: tsjPreset.transform,
-// coverageDirectory: './coverage',
-// testMatch: [
-// ***/?(*.)+(spec).ts'
-// ],
-// collectCoverageFrom: [
-// 'src/**/*.ts'
-// collectCoverage: true,
-// resetMocks: true,
-// clearMocks: true,
-// watchPathIgnorePatterns: ['globalConfig']
-// }
+}
